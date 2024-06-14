3d1184d6303cb979bb3594c30464b7cd88dcc97c
Prajwal Nikam <pn476964@gmail.com>
2024-06-14 13:06:22 +0530

    new file add

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+// # Variables : variables are containers for storing data/value.
+// # JavaScript variables can be declared in 4 ways :- Automatically, using var, using let, using const.
+
+// ## What is var, let and const ?
+// # var, let and const are the keywords used for declaring the variables.
+
+// # var is global scope.
+// # let is block scope.
+// # const is block scope and const cannot be Redeclared, cannot be Reassigne.
+
+var firstName = 'ram';
+let accountEmail = 'someone@gmail.com';
+const accountId = 532563;
+
+console.log(accountEmail);
+
+console.table([firstName, accountEmail, accountId]);
