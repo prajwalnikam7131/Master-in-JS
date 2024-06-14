b3ccce1b6b70d755f1b57117c27c8a779ee387dc
Prajwal Nikam <pn476964@gmail.com>
2024-06-14 17:14:39 +0530

    new file add

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+
+
+// ## Data Types : data types are used to define type of variables.
+
+// # JavaScript has 8 Data types: String, Number, BigInt (handles large Integer), Boolean, undefined, null, Symbol, Object.
+
+
+
+let str = 'hello';  // String
+let num = 24;       // Number
+let bool = true;    // boolean
+let firstName;      // undefined
+
+let obj = {
+    firstName: 'prajwal',
+    age: 24
+};                  // object
+
+
+// # null : The null represents the intentional absence of any object value.
+
+null === undefined; // false
+null == undefined;  // true
